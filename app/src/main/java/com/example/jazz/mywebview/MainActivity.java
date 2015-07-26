7c21ef74fd3f746bbc56da1a3d6481b7500ed431
unknown <david20jazz@gmail.com>
2015-07-26 10:33:09 +0900

    edit webview

---
+++ app/src/main/java/com/example/jazz/mywebview/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.jazz.mywebview;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 웹뷰 시작
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        // 웹뷰 끝
+    }
+
+    // 버튼 클릭했을때 네이버 주소 가져와서 뿌리기
+    public void onButton1Clicked(View v) {
+        webView.loadUrl("http://m.naver.com");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ readmeMeUseGit.txt
@@ -0,0 +1,6 @@
+echo # WebView >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/david20jazz/WebView.git
+git push -u origin master
